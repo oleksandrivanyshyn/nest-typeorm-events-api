67a0cd722fde20c7391c598d75e08dac60b8b2a7
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-27 01:22:36 +0300

    feat(auth): add userAdd mutation and support CreateUserDto in UserResolver

--- src/auth/user.resolver.ts
+++ src/auth/user.resolver.ts
@@ -1,14 +1,21 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from './user.entity';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuardJwtGql } from './auth-guard-jwt.gql';
 import { CurrentUser } from './current-user.decorator';
+import { UserService } from './user.service';
+import { CreateUserDto } from './input/create.user.dto';
 
 @Resolver(() => User)
 export class UserResolver {
+  constructor(private readonly userService: UserService) {}
   @Query(() => User, { name: 'me' })
   @UseGuards(AuthGuardJwtGql)
   public async me(@CurrentUser() user: User): Promise<User> {
     return user;
+  }
+  @Mutation(() => User, { name: 'userAdd' })
+  public async add(@Args('input') input: CreateUserDto): Promise<User> {
+    return await this.userService.create(input);
   }
 }
