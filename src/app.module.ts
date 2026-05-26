a600e04346db1b54160eec30cfb1339ef42d9149
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-05-26 20:31:27 +0300

    feat(auth): add authentication module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppDummy } from './app.dummy';
 import { AppJapanService } from './app.japan.service';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import ormConfig from './config/orm.config';
 import ormConfigProd from './config/orm.config.prod';
 
@@ -20,6 +21,7 @@ import ormConfigProd from './config/orm.config.prod';
       useFactory:
         process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [
